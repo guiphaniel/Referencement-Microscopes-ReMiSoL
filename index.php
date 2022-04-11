3aaeb7424acf4dac4472d69a9663c660b78d33b7
guiphaniel <50924532+guiphaniel@users.noreply.github.com>
2022-04-11 13:30:52 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil</title>
+</head>
+<body>
+    <header>
+        <h1>Accueil</h1>
+    </header>
+    <main>
+        <p>Bienvenu sur notre site !</p>
+    </main>
+    <footer>
+        <address>
+            <a href="mailto:xxx.xxx@XXX.fr">xxx.xxx@xxx.fr</a>
+        </address>
+    </footer>
+</body>
+</html>
