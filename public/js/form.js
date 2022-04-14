6203eb91b2ee4506624c4f5b1b9bd67545ffae90
guiphaniel <50924532+guiphaniel@users.noreply.github.com>
2022-04-14 09:45:25 +0200

    fix: bad input name

--- public/js/form.js
+++ public/js/form.js
@@ -17,7 +17,7 @@ function addMicroscopeField() {
         <label for="micro-ref-${id}">Référence</label>
         <input id="micro-ref-${id}" type="text" name="microscopes[${id}][microRef]" required>
         <label for="micro-rate-${id}">Tarification</label>
-        <input id="micro-rate-${id}" type="text" name="microscopes[${id}][microRate]}" required>
+        <input id="micro-rate-${id}" type="text" name="microscopes[${id}][microRate]" required>
         <label for="micro-desc-${id}">Description</label>
         <textarea id="micro-desc-${id}" name="microscopes[${id}][microDesc]" cols="30" rows="10" required></textarea>
     `;
