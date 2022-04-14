15aa5deb86e9144c5f5893b662367f7d3a67d13d
guiphaniel <50924532+guiphaniel@users.noreply.github.com>
2022-04-14 15:17:57 +0200

    Update form processing to handle new form

--- processing/form_processing.php
+++ processing/form_processing.php
@@ -1,26 +1,34 @@
 <?php
     include_once("../model/start_db.php");
-    include_once("../model/Microscope.php");
-    include_once("../model/Coordinates.php");
+    include_once("../model/entities/Lab.php");
+    include_once("../model/entities/Contact.php");
+    include_once("../model/entities/MicroscopesGroup.php");
+    include_once("../model/services/MicroscopeService.php");
 
-    //verify that all fields were sent by the form
-    if (!isset($_POST["labName"]) || !isset($_POST["microRef"]) || !isset($_POST["desc"]) || !isset($_POST["lat"]) || !isset($_POST["lon"])) {
+    // TODO: verifier que les laboratoires, les microscopes et les thèmes font bien parti de ceux déjà présent en bd
+    //verify that all fields were sent by the form TODO: if not, store values in session to prefill the form 
+    if (!isset($_POST["labName"]) || !isset($_POST["labAddress"]) || !isset($_POST["contactName"]) || !isset($_POST["contactEmail"]) || !isset($_POST["lat"]) || !isset($_POST["lon"])) {
         header('location: /form.php');
         exit();
     }
 
-    $microscope = new Microscope();
-    $microscope
-        ->setLabName($_POST["labName"])
-        ->setRef($_POST["microRef"])
-        ->setDesc($_POST["desc"])
-        ->setCoor(new Coordinates($_POST["lat"], $_POST["lon"]));
+    foreach($_POST["microscopes"] as $micro) {
+        if (!isset($micro["brand"]) || !isset($micro["ref"]) || !isset($micro["rate"]) || !isset($micro["desc"])) {
+            header('location: /form.php');
+            exit();
+        }
+    }
 
-    $sql = $pdo->prepare("INSERT INTO microscopes VALUES (NULL, :serialized)");
+    // Convert form values into objects...
+    $lab = new Lab($_POST["labName"], $_POST["labAddress"]);
+    $contact = new Contact($_POST["contactName"], $_POST["contactEmail"]);
+    $group = new MicroscopesGroup($_POST["lat"], $_POST["lon"], $lab, $contact);
 
-    $sql->execute([
-        'serialized' => serialize($microscope)
-    ]);
+    foreach($_POST["microscopes"] as $micro)
+        $group->addMicroscope(new Microscope($micro["brand"], $micro["ref"], $micro["rate"], $micro["desc"]));
+
+    // ...and save the group into db
+    MicroscopeService::getInstance()->saveGroup($group);
 
     header('location: /index.php');
 
