74fb38b1bbed28e5a96a4c43b424d51d38db33e8
guiphaniel <50924532+guiphaniel@users.noreply.github.com>
2022-04-20 11:52:35 +0200

    refactor: update sql schema (natural keys & fix relations)

--- config/schema.sql
+++ config/schema.sql
@@ -1,16 +1,11 @@
 BEGIN TRANSACTION;
-DROP TABLE IF EXISTS "theme";
-CREATE TABLE IF NOT EXISTS "theme" (
-	"type"	TEXT,
-	PRIMARY KEY("type")
-);
 DROP TABLE IF EXISTS "manage";
 CREATE TABLE IF NOT EXISTS "manage" (
 	"microscopes_group_id"	INTEGER,
 	"contact_id"	INTEGER,
-	PRIMARY KEY("microscopes_group_id","contact_id"),
 	FOREIGN KEY("microscopes_group_id") REFERENCES "microscopes_group"("id"),
-	FOREIGN KEY("contact_id") REFERENCES "contact"("id")
+	FOREIGN KEY("contact_id") REFERENCES "contact"("id"),
+	PRIMARY KEY("microscopes_group_id","contact_id")
 );
 DROP TABLE IF EXISTS "contact";
 CREATE TABLE IF NOT EXISTS "contact" (
@@ -20,23 +15,14 @@ CREATE TABLE IF NOT EXISTS "contact" (
 	"email"	TEXT UNIQUE,
 	PRIMARY KEY("id" AUTOINCREMENT)
 );
-DROP TABLE IF EXISTS "belong_theme";
-CREATE TABLE IF NOT EXISTS "belong_theme" (
-	"belong_microscopes_group_id"	INTEGER,
-	"belong_microscope_id"	INTEGER,
-	"theme_type"	INTEGER,
-	PRIMARY KEY("belong_microscopes_group_id","belong_microscope_id","theme_type"),
-	FOREIGN KEY("theme_type") REFERENCES "theme"("type"),
-	FOREIGN KEY("belong_microscopes_group_id","belong_microscope_id") REFERENCES "belong"("microscopes_group_id","microscope_id")
-);
 DROP TABLE IF EXISTS "belong";
 CREATE TABLE IF NOT EXISTS "belong" (
 	"microscopes_group_id"	INTEGER,
 	"microscope_id"	INTEGER,
 	"rate"	NUMERIC,
 	"desc"	TEXT,
-	FOREIGN KEY("microscope_id") REFERENCES "microscope"("id"),
 	FOREIGN KEY("microscopes_group_id") REFERENCES "microscopes_group"("id"),
+	FOREIGN KEY("microscope_id") REFERENCES "microscope"("id"),
 	PRIMARY KEY("microscopes_group_id","microscope_id")
 );
 DROP TABLE IF EXISTS "lab";
@@ -55,12 +41,44 @@ CREATE TABLE IF NOT EXISTS "microscopes_group" (
 	FOREIGN KEY("lab_id") REFERENCES "lab"("id") ON DELETE CASCADE,
 	CONSTRAINT "pk_microscope_group" PRIMARY KEY("id" AUTOINCREMENT)
 );
+DROP TABLE IF EXISTS "belong_keyword";
+CREATE TABLE IF NOT EXISTS "belong_keyword" (
+	"belong_microscopes_group_id"	INTEGER,
+	"belong_microscope_id"	INTEGER,
+	"keyword_id"	INTEGER,
+	FOREIGN KEY("keyword_id") REFERENCES "keyword"("cat"),
+	FOREIGN KEY("belong_microscopes_group_id","belong_microscope_id") REFERENCES "belong"("microscopes_group_id","microscope_id"),
+	PRIMARY KEY("belong_microscopes_group_id","belong_microscope_id","keyword_id")
+);
+DROP TABLE IF EXISTS "keyword";
+CREATE TABLE IF NOT EXISTS "keyword" (
+	"id"	INTEGER,
+	"cat"	TEXT,
+	"tag"	TEXT,
+	PRIMARY KEY("id" AUTOINCREMENT),
+	UNIQUE("cat","tag")
+);
+DROP TABLE IF EXISTS "controller";
+CREATE TABLE IF NOT EXISTS "controller" (
+	"name"	TEXT,
+	"brand_name"	TEXT,
+	FOREIGN KEY("brand_name") REFERENCES "brand"("name") ON DELETE CASCADE,
+	PRIMARY KEY("name")
+);
+DROP TABLE IF EXISTS "brand";
+CREATE TABLE IF NOT EXISTS "brand" (
+	"name"	TEXT,
+	"compagny"	TEXT,
+	PRIMARY KEY("name")
+);
 DROP TABLE IF EXISTS "microscope";
 CREATE TABLE IF NOT EXISTS "microscope" (
 	"id"	INTEGER,
-	"brand"	TEXT,
-	"ref"	TEXT,
-	CONSTRAINT "u_brand_ref" UNIQUE("brand","ref"),
-	CONSTRAINT "pk_microscope" PRIMARY KEY("id" AUTOINCREMENT)
+	"model"	TEXT,
+	"controller_name"	TEXT NOT NULL,
+	"brand_name"	TEXT,
+	FOREIGN KEY("controller_name") REFERENCES "controller"("name"),
+	FOREIGN KEY("brand_name") REFERENCES "brand"("name") ON DELETE CASCADE,
+	PRIMARY KEY("id" AUTOINCREMENT)
 );
 COMMIT;
