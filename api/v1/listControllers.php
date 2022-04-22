b9842fab58cad5ad6016d65889ffb39ad3b887ad
guiphaniel <50924532+guiphaniel@users.noreply.github.com>
2022-04-22 11:57:04 +0200

    REST API add lists

---
+++ api/v1/listControllers.php
@@ -0,0 +1,30 @@
+<?php
+    include_once("../../model/services/ControllerService.php");
+
+    $request_method = $_SERVER["REQUEST_METHOD"];
+
+    switch($request_method)
+    {
+        case 'GET':
+            //TODO: parametres (limit, offset) TODO: renvoyer la liste des groupes de microscopes (séparer les requetes SQL, et convertir en objets)
+            //TODO: if no controller is provided, return all controllers -> change the behavior of the ControllerService::getAllControllers method, with optional parameter
+            if(!isset($_GET["compagny"])) {
+                echo "You must provide a compagny name : ?compagny=compagnyName";
+                die();
+            }
+            
+            if(!isset($_GET["brand"])) {
+                echo "You must provide a brand name : &brand=brandName";
+                die();
+            }
+
+            $controllers = ControllerService::getInstance()->getAllControllers(new Brand($_GET["brand"], new Compagny($_GET["compagny"])));
+            
+            header('Content-Type: application/json');
+            echo json_encode($controllers, JSON_PRETTY_PRINT);
+            break;
+        default:
+            // Requête invalide
+            header("HTTP/1.0 405 Method Not Allowed");
+            break;
+    }
