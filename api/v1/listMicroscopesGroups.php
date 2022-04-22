3e9ff6f3e020157322c591d1d879b623984df60a
guiphaniel <50924532+guiphaniel@users.noreply.github.com>
2022-04-22 10:36:42 +0200

    update REST API to list microscopes groups

---
+++ api/v1/listMicroscopesGroups.php
@@ -0,0 +1,22 @@
+<?php
+    include_once("../../model/start_db.php");
+    include_once("../../model/services/MicroscopesGroupService.php");
+
+    $request_method = $_SERVER["REQUEST_METHOD"];
+
+    switch($request_method)
+    {
+        case 'GET':
+            //TODO: parametres (limit, offset) TODO: renvoyer la liste des groupes de microscopes (séparer les requetes SQL, et convertir en objets)
+            
+            $groups = MicroscopesGroupService::getInstance()->getAllMicroscopesGroup();
+            
+            header('Content-Type: application/json');
+            echo json_encode($groups, JSON_PRETTY_PRINT);
+            break;
+        default:
+            // Requête invalide
+            header("HTTP/1.0 405 Method Not Allowed");
+            break;
+    }
+
