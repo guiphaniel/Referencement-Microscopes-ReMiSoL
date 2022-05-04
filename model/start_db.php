cea259ee22684d0354d4894236b21848e2ae6c0f
guiphaniel <50924532+guiphaniel@users.noreply.github.com>
2022-05-04 16:07:10 +0200

    fix: enable foreign keys

--- model/start_db.php
+++ model/start_db.php
@@ -8,3 +8,4 @@ $dsn = 'sqlite:' . __DIR__ . '/database.db';
         }
 
         $pdo->setAttribute(PDO::ATTR_ERRMODE , PDO::ERRMODE_EXCEPTION );
+        $pdo->exec( 'PRAGMA foreign_keys = ON;' );
