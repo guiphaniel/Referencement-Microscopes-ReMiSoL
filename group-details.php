500a9b75822a14f6afc37917385354ed99506544
guiphaniel <50924532+guiphaniel@users.noreply.github.com>
2022-05-12 11:15:34 +0200

    use GroupDetailsCreator

--- group-details.php
+++ group-details.php
@@ -1,6 +1,7 @@
 <?php 
     include_once("model/services/MicroscopesGroupService.php");
     include_once("view/generators/HeaderCreator.php");
+    include_once("view/generators/GroupDetailsCreator.php");
 
     if(empty($_GET["id"])) {
         header('location: /index.php');
@@ -35,81 +36,7 @@
         $header->create()
     ?>
     <main>
-        <section>
-            <h2><?= $group->getLab()->getName() . " (" . $group->getLab()->getCode() . ")"; ?></h2>
-            <p><?= nl2br($group->getLab()->getAddress()->toString()); ?></p>
-            <p>Site internet : <a href="<?= $group->getLab()->getWebsite(); ?>"><?= $group->getLab()->getWebsite(); ?></a></p>
-            <div id="map" style="height: 400px;"></div>
-        </section>
-        <section>
-            <h2>Référent·e·s</h2>
-            <?php foreach ($group->getContacts() as $contact) : ?>
-                <address>
-                    <p><?= $contact->getFirstname() . ' ' . $contact->getLastname() . " (" . $contact->getRole() .")" ?></p>
-                    <p>Email : <a href="mailto:<?= $contact->getEmail() ?>"><?= $contact->getEmail() ?></a></p>
-                    <?php $phone = $contact->getPhoneCode() . $contact->getPhoneNum(); ?>
-                    <p>Téléphone : <a href="tel:<?= $phone ?>"><?= $phone ?></a></p>
-                </address>
-            <?php endforeach ?>
-        </section>
-        <section>
-            <h2>
-                Microscopes
-            </h2>
-            <?php foreach ($group->getMicroscopes() as $micro) : 
-                $ctr = $micro->getController();
-                $model = $micro->getModel();
-                $brand = $model->getBrand();
-                $compagny = $brand->getCompagny();
-                $type = match ($micro->getType()) {
-                    "LABO" => "laboratoire",
-                    "PLAT" => "plateforme"
-                };
-                $name = implode(" - ", [$compagny->getName(), $brand->getName(), $model->getName(), $ctr->getName()]);
-                ?>
-                <section>
-                    <h3><?= $name . " (" . $type . ")"; ?></h3>
-                    <?php 
-                        $microId = $micro->getId();
-                        $path = glob(__DIR__ . "/public/img/micros/" . "$microId.*")[0]??false;
-
-                        if($path):
-                            $fileName = substr($path, strrpos($path, "/") + 1); 
-                    ?>
-                        <img class="micro-img" src="/public/img/micros/<?=$fileName?>" alt="Microscope <?=$name?>">
-                    <?php endif; ?>
-                    <p>Description : <?= $micro->getDesc(); ?></p>
-                    <?php if(!empty($micro->getRate())) : ?>
-                        <p>Tarification : <a href="<?= $micro->getRate(); ?>"><?= $micro->getRate(); ?></a></p>
-                    <?php endif; ?>
-                    <?php 
-                    $access = $micro->getAccess();
-                    if($access == "BOTH" || $access == "ACAD") : ?>
-                        <p>Ouvert aux académiques</p>
-                    <?php endif;
-                    if($access == "BOTH" || $access == "INDU") : ?>
-                        <p>Ouvert aux industriels</p>
-                    <?php endif; ?>
-                    <table>
-                        <caption>Mots-clés</caption>
-                        <thead>
-                            <tr>
-                                <th scope="colgroup">Catégories</th>
-                                <th scope="colgroup">Étiquettes</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php foreach ($micro->getKeywords() as $cat => $tags) : ?>
-                                <tr>
-                                    <th scope="rowgroup"><?= $cat; ?></th>
-                                    <td><?= implode(", ", $tags); ?></td>
-                                </tr>
-                            <?php endforeach; ?>
-                        </tbody>
-                    </table>
-                </section>
-            <?php endforeach ?>
-        </section>
+        <?php (new GroupDetailsCreator($group, true))->create() ?>
     </main>
     <footer>
         <address>
