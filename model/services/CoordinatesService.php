9ef40795e5dee8f48284fb7bfc2ae48f0770527a
guiphaniel <50924532+guiphaniel@users.noreply.github.com>
2022-05-13 19:46:06 +0200

    Create CoordinatesService.php

---
+++ model/services/CoordinatesService.php
@@ -0,0 +1,71 @@
+<?php
+    include_once(__DIR__ . "/../start_db.php");
+    include_once(__DIR__ . "/../entities/Coordinates.php");
+
+    spl_autoload_register(function ($class_name) {
+        include $class_name . '.php';
+    });
+    
+    class CoordinatesService extends AbstractService {
+        static private $instance;
+
+        static function getInstance() : CoordinatesService {
+            if(!isset(self::$instance))
+                self::$instance = new CoordinatesService();
+           
+            return self::$instance;
+        }
+
+        function getCoordinatesId(Coordinates $coor) {
+            global $pdo;
+
+            $sth = $pdo->prepare("SELECT id FROM coordinates where lat = :lat and lon = :lon");
+
+            $sth->execute([
+                "lat" => $coor->getLat(), 
+                "lon" => $coor->getLon()
+            ]);
+
+            $row = $sth->fetch();
+
+            // if this coordinates exist, return their id, else return -1
+            return $row ? $row[0] : -1;
+        }
+
+        function findCoordinatesById($id) : Coordinates {
+            global $pdo;
+
+            $sql = "
+                select lat, lon
+                from coordinates as c
+                where c.id = $id
+            ";
+
+            $sth = $pdo->query($sql);
+            $coorInfos = $sth->fetch(PDO::FETCH_NAMED);
+
+            return (new Coordinates($coorInfos["lat"], $coorInfos["lon"]))->setId($id);
+        }
+
+        /** Saves the coordinates if they don't exist yet, and returns their id */
+        function save(Coordinates $coor) : int {
+            global $pdo;
+            
+            $id = $this->getCoordinatesId($coor);
+            
+            // if the coordinates isn't already in the db, add it
+            if ($id == -1)  {
+                $sth = $pdo->prepare("INSERT INTO coordinates VALUES (NULL, :lat, :lon)");
+
+                $sth->execute([
+                    "lat" => $coor->getLat(), 
+                    "lon" => $coor->getLon(),
+                ]);
+
+                $id = $pdo->lastInsertId();
+                $coor->setId($id);
+            }          
+
+            return $id;
+        }
+    }            
