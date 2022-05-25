610699ca4396790e4d25cf551ad1fc8a066378bb
guiphaniel <50924532+guiphaniel@users.noreply.github.com>
2022-05-25 20:04:12 +0200

    add REGEXP & CONCAT UDFs for sqlite db

--- model/start_db.php
+++ model/start_db.php
@@ -1,11 +1,24 @@
 <?php
 $dsn = 'sqlite:' . __DIR__ . '/database.db';
-        try {    
-            $pdo = new PDO($dsn);
-        } catch (PDOException $e) {
-            echo 'Connexion échouée : ' . $e->getMessage();
-            die();
-        }
 
-        $pdo->setAttribute(PDO::ATTR_ERRMODE , PDO::ERRMODE_EXCEPTION );
-        $pdo->exec( 'PRAGMA foreign_keys = ON;' );
+try {    
+    $pdo = new PDO($dsn);
+} catch (PDOException $e) {
+    echo 'Connexion échouée : ' . $e->getMessage();
+    die();
+}
+
+$pdo->setAttribute(PDO::ATTR_ERRMODE , PDO::ERRMODE_EXCEPTION );
+$pdo->exec( 'PRAGMA foreign_keys = ON;' );
+
+$pdo->sqliteCreateFunction("REGEXP", "REGEXP", 2);
+$pdo->sqliteCreateFunction("CONCAT", "CONCAT");
+
+function REGEXP($pattern, $subject) {
+    return preg_match("/" . $pattern . "/", $subject);
+}
+
+function CONCAT(...$strings) {
+    return implode("", $strings);
+}
+
