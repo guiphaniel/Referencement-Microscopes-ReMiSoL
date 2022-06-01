ce850d3edf5fb68e93e65b5e023eab775e3b9963
guiphaniel <50924532+guiphaniel@users.noreply.github.com>
2022-06-01 20:04:31 +0200

    fix: MySQL REGEXP needs escaping twice

--- model/services/MicroscopesGroupService.php
+++ model/services/MicroscopesGroupService.php
@@ -114,7 +114,7 @@
                 if(!$includeLocked)
                     $sql .= "where g.id not in (select microscopes_group_id from locked_microscopes_group)";
             } else {
-                $sqlFilters = "(" .  implode("|", array_map(function ($filter) { return preg_quote($filter); }, $filters)) . ")";
+                $sqlFilters = "(" .  implode("|", array_map(function ($filter) { $quote = preg_quote($filter); if(MY_DBMS == DBMS::MySQL) $quote = preg_quote($quote); return $quote; }, $filters)) . ")"; // MySQL needs to escape parentheses, for exemple, two times => \\(
                 $sql = "
                     SELECT groupId, coorId, labId, microId, concat from (
                         select g.id as groupId, coordinates_id as coorId, lab.id as labId, mi.id as microId, CONCAT(GROUP_CONCAT(DISTINCT con.norm_lastname), GROUP_CONCAT(DISTINCT c.norm_name), GROUP_CONCAT(DISTINCT norm_tag), LOWER(mo.name), LOWER(ctr.name), LOWER(b.name), LOWER(cmp.name), mi.norm_descr) as concat
