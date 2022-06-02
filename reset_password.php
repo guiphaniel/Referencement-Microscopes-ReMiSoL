7c96c25e32394786294f824192a61854c5c2009d
guiphaniel <50924532+guiphaniel@users.noreply.github.com>
2022-06-02 18:31:34 +0200

    add stylesheet

--- reset_password.php
+++ reset_password.php
@@ -32,6 +32,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/public/css/style.css">
     <title>Réinitialisation du mot de passe</title>
 </head>
 <body>
